2290a940eac6bd2ce5aff51e0c13fa42d789aef7
Justin Schellenberg <jrschellenberg@gmail.com>
2020-05-12 16:33:57 -0500

    Extract Clean data to own file

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from constants import TEAMS, PLAYERS
-
-def unpack_player(name, guardians, experience, height):
-    return (name, guardians, experience, height)
-
-
-def clean_data(players):
-    players_copy = []
-    for p in players:
-        player = p.copy()
-        name, guardians, experience, height = unpack_player(**p)
-        print(name)
+from clean_data import clean_data
 
 
 def init():
     teams = TEAMS.copy()
     players = clean_data(PLAYERS)
+    print(players)
 
 
 if __name__ == '__main__':
---
+++ clean_data.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def __convert_height_to_int(string):
+    try:
+        [integer, *_] = string.split()
+        return int(integer)
+    except ValueError:
+        raise ValueError
+
+
+def __convert_exp_to_bool(string):
+    try:
+        if string.lower() != 'yes' and string.lower() != 'no':
+            raise ValueError
+        return string.lower() == 'yes'
+    except ValueError:
+        raise ValueError("Oops! There was an issue "
+                         "converting experience to a Bool,"
+                         " Please ensure it is 'yes' or 'no' "
+                         "only and try again")
+
+
+def clean_data(players):
+    players_dict = {}
+    players_list = []
+
+    for player in players.copy():
+        try:
+            (key1, experience), \
+                (key2, guardians), \
+                (key3, height), \
+                (key4, name) = sorted(player.items())
+
+            players_dict[key1] = __convert_exp_to_bool(experience)
+            players_dict[key2] = guardians.split(' and ')
+            players_dict[key3] = __convert_height_to_int(height)
+            players_dict[key4] = name
+            players_list.append(players_dict)
+
+        except ValueError as err:
+            print(err)
+            print("Please fix data in constants.py and please try again")
+            sys.exit()
+
+    return players_list
